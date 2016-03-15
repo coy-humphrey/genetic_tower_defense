b72a2a0b46b96cd4a635ca7badc97e981bd783f6
Coy Humphrey <cmhumphr@ucsc.edu>
2016-03-15 12:53:24 -0700

    added vector normalization

--- pygame_test/genetics.py
+++ pygame_test/genetics.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def crossover (array1, array2):
 	return [random.choice(t) for t in zip(array1, array2)]
@@ -8,3 +9,7 @@ def mutate (array1):
 	i = random.randint(0, len(array1) - 1)
 	a[i] = random.random()
 	return a
+
+def normalize (a):
+	magnitude = math.sqrt (sum ([x**2 for x in a]))
+	return [x / magnitude for x in a]
